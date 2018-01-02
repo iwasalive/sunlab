bffdf93f5dd5e8ffda8e2042236dc627c202d050
Kessy Similien <simkessy@gmail.com>
2018-01-02 00:55:21 -0500

    various

--- ansible/roles/db/files/sql-setup.ps1
+++ ansible/roles/db/files/sql-setup.ps1
@@ -1,2 +1,12 @@
 # Install SQL Server
-c:\temp\sql-server-2017.exe /ConfigurationFile="C:\temp\sql-config.ini" /MediaPath="C:\Temp\SQL" /iacceptsqlserverlicenseterms /indicateprogress=false /q
+c:\temp\sql-server-2017.exe /ConfigurationFile="C:\temp\sql-config.ini" /MediaPath="C:\Temp\SQL" /iacceptsqlserverlicenseterms /indicateprogress=false /Q
+
+# Create DB
+# SQLPS
+# Set-Location SQLSERVER:\SQL
+
+# cd $env:COMPUTERNAME
+# $srv = Get-Item $database
+
+# $db = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -argumentlist $srv, "eb-${environment}-sc1"
+# $db.create()
---
+++ ansible/roles/eb/files/tasks.ps1
@@ -0,0 +1,36 @@
+# msiexec.exe /log logfile-projectwise.txt /i "C:\software\BentleyDownloads\ebi150601115en\Server Installation\eB Engineering Designer Database Files\eB Engineering Designer Database Files (x64).msi" /qn /passive
+
+# msiexec.exe /log logfile-projectwise.txt /i "C:\Users\sunlab\
+# Downloads\BentleyDownloads\ebi150601115en\Server Installation\eB ProjectWise Connector\eB ProjectWise Connector (x64).msi" /qn /passive
+
+# msiexec.exe /log logfile-projectwise.txt /i "C:\Users\sunlab\
+# Downloads\BentleyDownloads\ebi150601115en\Server Installation\eB SSIS Components\eB SSIS Components (x64).msi" /qn /passive
+
+
+# Stop eb 
+Stop-Service "eB Service Manager"
+
+# Get all apps
+$comAdmin = New-Object -com ("COMAdmin.COMAdminCatalog.1")
+$applications = $comAdmin.GetCollection("Applications") 
+$applications.Populate() 
+
+# Shut down and disable
+$ebInsightApp = $applications | ? {$_.name -eq "eB Insight"}
+$comAdmin.ShutdownApplication("eB Insight")
+$ebInsightApp.Value("IsEnabled") = $false
+$applications.SaveChanges()
+
+# Enable application and start
+$ebInsightApp.Value("IsEnabled") = $true
+$comAdmin.StartApplication("eB Insight")
+$applications.SaveChanges()
+
+# eB Shared Path
+$ebSharedPath = "C:\Program Files\Bentley\eB\Shared"
+
+# Call Scripts 
+
+Invoke-sqlcmd -ServerInstance $server -Database $db -InputFile $filename
+
+$listOfFiles | % { Invoke-sqlcmd -ServerInstance $server -Database $db -InputFile $_ } 
