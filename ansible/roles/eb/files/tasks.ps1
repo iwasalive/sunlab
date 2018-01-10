7e2c2bc8f77af1a227bcf8059550566e25fe3e77
Kessy <simkessy@gmail.com>
2018-01-09 20:29:14 -0500

    ps updates

--- ansible/roles/eb/files/tasks.ps1
+++ ansible/roles/eb/files/tasks.ps1
@@ -1,7 +1,7 @@
 # msiexec.exe /log logfile-projectwise.txt /i "C:\software\BentleyDownloads\ebi150601115en\Server Installation\eB Engineering Designer Database Files\eB Engineering Designer Database Files (x64).msi" /qn /passive
 
 # msiexec.exe /log logfile-projectwise.txt /i "C:\Users\sunlab\
-# Downloads\BentleyDownloads\ebi150601115en\Server Installation\eB ProjectWise Connector\eB ProjectWise Connector (x64).msi" /qn /passive
+# Downloads\BentleyDownloads\ebi150601115en\Server Installation\eB ProjectWise Connector\eB ProjectWise Database Files (x64).msi" /qn /passive
 
 # msiexec.exe /log logfile-projectwise.txt /i "C:\Users\sunlab\
 # Downloads\BentleyDownloads\ebi150601115en\Server Installation\eB SSIS Components\eB SSIS Components (x64).msi" /qn /passive
@@ -34,3 +34,12 @@ $ebSharedPath = "C:\Program Files\Bentley\eB\Shared"
 Invoke-sqlcmd -ServerInstance $server -Database $db -InputFile $filename
 
 $listOfFiles | % { Invoke-sqlcmd -ServerInstance $server -Database $db -InputFile $_ } 
+
+$iisAppName = "MyApp"
+
+# IIS disable anon auth and windows auth 
+Write-Host Disable anonymous authentication
+Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/anonymousAuthentication" -Name Enabled -Value False -PSPath IIS:\ -Location "Default Web Site/$iisAppName"
+
+Write-Host Enable windows authentication
+Set-WebConfigurationProperty -Filter "/system.webServer/security/authentication/windowsAuthentication" -Name Enabled -Value True -PSPath IIS:\ -Location "Default Web Site/$iisAppName"
